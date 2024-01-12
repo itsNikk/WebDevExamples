70839ae1488ce961c1389daa8f0697e3fc9a8855
Nicolò Buganza <nicolo.buganza@tiscali.it>
2024-01-12 18:39:05 +0100

    Update LanguageFeatures.js

--- WebDevExamplesProject/5BI/HTMLIntro/LanguageFeatures.js
+++ WebDevExamplesProject/5BI/HTMLIntro/LanguageFeatures.js
@@ -17,7 +17,7 @@ console.log("Il tipo di variable è " + typeof variable);
 console.log(typeof boolean);
 
 function saluta(nome) {
-    console.log("Hello, "+nome);
+    console.log("Hello, " + nome);
 }
 
 saluta("Luca")
@@ -26,51 +26,52 @@ saluta(3)
 
 // le funzioni sono tipi ;)
 let convertiInMiglia = function miglia(km) {
-    
+
     return km * 0.6214
-    
+
 }
 
 console.log(convertiInMiglia(45));
 
 // calcolatrice one-line
-function calcola(a,b,op) {
-    return op(a,b)
+function calcola(a, b, op) {
+    return op(a, b)
 }
 
-function somma(a,b) {
-    return a+b;
+function somma(a, b) {
+    return a + b;
 }
+
 //funzioni come parametri
-console.log(calcola(23,3,somma));
+console.log(calcola(23, 3, somma));
 // funzioni anonime
-console.log(calcola(4,5, (a,b) => a-b))
-console.log(calcola(4,5, (a,b) => a*b))
+console.log(calcola(4, 5, (a, b) => a - b))
+console.log(calcola(4, 5, (a, b) => a * b))
 
 // IF
 console.log(1 == '1');
 console.log(5 == '6');
 
-let array = [1,2,3,"luca", [1,2,3]]
-let arraCtr = new Array(1,2,5,3)
+let array = [1, 2, 3, "luca", [1, 2, 3]]
+let arraCtr = new Array(1, 2, 5, 3)
 
 // dowhile, while, for
 // foreach in JS
-for(let elem of array) {
+for (let elem of array) {
     console.log(elem);
 }
 
 //Oggetti in js
 let myObj = {
-    name:"Nicolò",
-    surname:"Buganza"
+    name: "Nicolò",
+    surname: "Buganza"
 }
 
 console.log(myObj);
 
 // for-in, for specifico per oggetti
-for(let elem in myObj){
-    console.log(elem + " "+ myObj[elem]);
+for (let elem in myObj) {
+    console.log(elem + " " + myObj[elem]);
 }
 
 //DOM BOM
