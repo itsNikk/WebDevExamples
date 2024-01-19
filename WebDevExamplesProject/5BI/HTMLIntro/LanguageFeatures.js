d1aa230f0b781000246839260d063ec089143dbd
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2024-01-19 15:40:33 +0100

    Update LanguageFeatures.js

--- WebDevExamplesProject/5BI/HTMLIntro/LanguageFeatures.js
+++ WebDevExamplesProject/5BI/HTMLIntro/LanguageFeatures.js
@@ -55,11 +55,46 @@ console.log(5 == '6');
 let array = [1, 2, 3, "luca", [1, 2, 3]]
 let arraCtr = new Array(1, 2, 5, 3)
 
+// metodi degli array
+//inserimento valore in coda
+let newArray = [4,74,568,123,9]
+
+newArray.push(10)
+//ins in testa
+newArray.unshift(0)
+
+// delete dalla coda
+newArray.pop()
+
+//delete dalal testa
+newArray.shift()
+
+console.log(newArray);
+
+console.log("\n");
+
+//ordinare array
+console.log("Ordinamento array");
+console.log(newArray.sort(
+
+    function (a,b) {
+        if(a<b) return -1
+        if(a>b) return 1
+        return 0
+    }
+
+));
+
+
+
+
 // dowhile, while, for
 // foreach in JS
 for (let elem of array) {
     console.log(elem);
 }
+
+
 
 //Oggetti in js
 let myObj = {
@@ -73,5 +108,31 @@ console.log(myObj);
 for (let elem in myObj) {
     console.log(elem + " " + myObj[elem]);
 }
+
+// Functional Programming in JS
+console.log("\n");
+console.log("Functional Programming in JS");
+
+let simpleArray = [1,2,3,4,5,6,7,8,9]
+
+// imperativa
+/*let sum=0
+for (const elem of simpleArray) {
+    sum+= elem*2
+}
+*/
+
+// MAP - trasforma ogni elem in un altro elemento
+const simpleDoubleArray = simpleArray.map(num => num*2)
+console.log(simpleDoubleArray);
+
+// FILTER - applica filtro su struttura
+const onlyEvenNumbers = simpleArray.filter(n=>n%2===0) 
+console.log(onlyEvenNumbers);
+
+// REDUCE - riduce la struttura ad un solo valore
+let tot = simpleArray.reduce((acc,num)=> acc+num,0)
+console.log(tot);
+
 
 //DOM BOM
