9424945b22b3f0ec767405741d1a5ea2fe8e19f9
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2024-02-02 11:52:08 +0100

    Update ManageSubmission.js

--- WebDevExamplesProject/VariousTests/ParamPassing_viaURL/scripts/ManageSubmission.js
+++ WebDevExamplesProject/VariousTests/ParamPassing_viaURL/scripts/ManageSubmission.js
@@ -7,11 +7,11 @@ document.getElementById('myForm').addEventListener("submit", function(event){
 
 function GetParamsAndRedirect() {
     // Ottieni i valori dai campi del modulo
-    var parametro1 = document.getElementById("parametro1").value;
-    var parametro2 = document.getElementById("parametro2").value;
+    let parametro1 = document.getElementById("parametro1").value;
+    let parametro2 = document.getElementById("parametro2").value;
 
     // Costruzione l'URL con i parametri
-    var urlDestinazione = "pagina2.html?parametro1=" 
+    let urlDestinazione = "pagina2.html?parametro1=" 
     + encodeURIComponent(parametro1) + "&parametro2=" 
     + encodeURIComponent(parametro2);
 
