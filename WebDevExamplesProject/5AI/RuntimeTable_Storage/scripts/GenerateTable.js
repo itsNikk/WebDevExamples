a42ec1218cfb8d1873595827c0e12a4ca44386c8
Nicolò Buganza <nicolo.buganza@tiscali.it>
2024-02-08 21:03:40 +0100

    Added runtime table creation solutions examples

---
+++ WebDevExamplesProject/5AI/RuntimeTable_Storage/scripts/GenerateTable.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    let datiSessionStorage = sessionStorage.getItem('datiTabella');
+    const dati = datiSessionStorage ? JSON.parse(datiSessionStorage) : [];
+
+    if (dati.length === 0) {
+        alert('Nessun dato disponibile. Torna alla pagina precedente per crearne qualcuno.');
+        return;
+    }
+
+    const table = document.createElement('table');
+
+    // Headings
+    const headerRow = table.insertRow();
+    for (const key in dati[0]) {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = key;
+        headerRow.appendChild(headerCell);
+    }
+
+    // insert data
+    for (const record of dati) {
+        const row = table.insertRow();
+        for (const key in record) {
+            const cell = row.insertCell();
+            cell.textContent = record[key];
+        }
+    }
+
+    document.body.appendChild(table);
+});
