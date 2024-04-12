00152629a8ef1ac74be819f108cac6221629b175
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2024-04-12 11:11:03 +0200

    Update fetchExample.js

--- WebDevExamplesProject/5AI/AsyncExamples/FetchExamples/fetchExample.js
+++ WebDevExamplesProject/5AI/AsyncExamples/FetchExamples/fetchExample.js
@@ -2,7 +2,8 @@ const url = "https://jsonplaceholder.typicode.com/users"
 
 async function getAllUsers(url) {
     let promise = await fetch(url)
-    if (promise.status === 200) {
+    // promise.ok uguale a promise.status === 200
+    if (promise.ok) {
         let jsonValue = await promise.json()
         console.log(jsonValue)
     } else {
