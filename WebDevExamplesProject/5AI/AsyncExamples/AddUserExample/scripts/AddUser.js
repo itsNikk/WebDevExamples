17b1d900ba79c988ddea55d52f2e996687ed0019
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2024-05-03 09:58:50 +0200

    Added AddUser via Fetch

---
+++ WebDevExamplesProject/5AI/AsyncExamples/AddUserExample/scripts/AddUser.js
@@ -0,0 +1,53 @@
+const addUserURL = "https://dummyjson.com/users/add"
+
+let userNameFromForm = '';
+let userLastNameFromForm = '';
+let userAge;
+
+document.getElementById("myForm").addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    userNameFromForm = document.getElementById("firstName").value
+    userLastNameFromForm = document.getElementById("lastName").value
+    userAge = document.getElementById("age").value
+
+    AddNewUser(addUserURL)
+})
+
+async function AddNewUser(addUserURL) {
+
+    let newUserObj = {
+        firstName: "",
+        lastName: "",
+        age: '',
+        gender: '',
+        birthDate: '',
+        address: {}
+    }
+
+    newUserObj.firstName = userNameFromForm
+    newUserObj.lastName = userLastNameFromForm
+    newUserObj.age = userAge
+    newUserObj.gender = GetUserGender()
+    newUserObj.birthDate = document.getElementById("birthDate").value
+
+    let response = await fetch(addUserURL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newUserObj)
+    })
+
+    let newUserData = await response.json()
+    console.log(newUserData)
+}
+
+function GetUserGender() {
+
+    /*
+    if(document.getElementById("M").checked){
+        retrun "M"
+    } else return "F"
+    */
+    //Operatore ternario
+    return document.getElementById("M").checked ? "M" : "F"
+}
